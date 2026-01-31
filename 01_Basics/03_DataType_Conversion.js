5116e315ad6ddb416910efd12b8ecf943c888e92
DEB108-Nitai <devranjanpramanick@gmail.com>
2026-01-31 14:47:54 +0000

    DataType Conversion

---
+++ 01_Basics/03_DataType_Conversion.js
@@ -0,0 +1,31 @@
+let score = "33"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score )
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33" after converting to Number : 33
+//"33abc" after converting to Number : NaN 
+// true => 1 , false => 0 
+
+let isLoggedIn = "Hare Krishna "
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+
+console.log(typeof booleanLoggedIn)
+console.log(booleanLoggedIn)
+
+// 1 => true , 0 => false 
+// "" => false 
+// "Nitai" => true 
+
+let someNumber = 108
+
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber)
+console.log(stringNumber)
